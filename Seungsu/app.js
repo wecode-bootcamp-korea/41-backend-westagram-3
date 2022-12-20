bdbc91b23c5f8cf98e58bcb99d698c94077a22c5
김승수 <admin@SeungSu.local>
2022-12-20 10:56:58 +0900

    초기환경세팅 TypeORM추가

--- Seungsu/app.js
+++ Seungsu/app.js
@@ -4,13 +4,32 @@ const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 
+dotenv.config();
+
+const { DataSource } = require("typeorm");
+
+const myDataSource = new DataSource({
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+});
+
+myDataSource
+  .initialize()
+  .then(() => {
+    console.log("Data Source has been init");
+  })
+  .catch(() => {
+    console.log("Promise Rejected!");
+  });
 app = express();
 
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
-
-dotenv.config();
 
 app.get("/ping", (req, res) => {
   res.json({ message: "pong" });
