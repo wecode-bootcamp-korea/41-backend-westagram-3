3724b6b34c26816c67db9ed319731ee92697f475
JongHoB <qorwhdgh@gmail.com>
2022-12-20 13:24:14 +0900

    Express 초기 환경세팅

---
+++ JongHo/app.js
@@ -0,0 +1,36 @@
+const express=require('express');
+const cors=require('cors');
+const dotenv=require('dotenv');
+const morgan=require('morgan');
+const {appendFile}=require('fs');
+dotenv.config();
+const {DataSource}=require('typeorm');
+const myDataSource=new DataSource({
+    type:process.env.TYPEORM_CONNECTION,
+    host:process.env.TYPEORM_HOST,
+    port:process.env.TYPEORM_PORT,
+    username:process.env.TYPEORM_USERNAME,
+    password:process.env.TYPEORM_PASSWORD,
+    database:process.env.TYPEORM_DATABASE
+});
+myDataSource.initialize().then(()=>{
+    console.log("DataSource initialized");
+});
+
+const app=express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan('combined'));
+
+app.get("/ping",(request, response)=>
+{
+    response.send("pong");
+});
+
+const PORT=process.env.PORT;
+
+app.listen(PORT,()=>
+{
+    console.log(`Server listening on port ${PORT}`);
+}
+);
