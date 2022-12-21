cd9c3892ad258a1d532643d59a21cfd793883198
김승수 <admin@SeungSu.local>
2022-12-21 13:08:28 +0900

    회원가입 엔드포인트 생성

---
+++ Seungsu/db/migrations/20221221014751_create_users_tables.sql
@@ -0,0 +1,13 @@
+-- migrate:up
+create table users (
+  id int not null auto_increment primary key,
+  name varchar(50) not null,
+  email varchar(100) not null,
+  password varchar(100) not null,
+  profile_image varchar(100) not null,
+  created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+  updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp
+);
+
+-- migrate:down
+drop table users;
