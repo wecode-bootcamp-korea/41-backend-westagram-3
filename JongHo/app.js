8cd68301102fd0e54f407a86d10e3fbc148d66d8
JongHoB <qorwhdgh@gmail.com>
2022-12-21 10:01:21 +0900

    Express 초기 환경세팅

--- JongHo/app.js
+++ JongHo/app.js
@@ -1,36 +1,48 @@
-const express=require('express');
-const cors=require('cors');
-const dotenv=require('dotenv');
-const morgan=require('morgan');
-const {appendFile}=require('fs');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const { appendFile } = require("fs");
 dotenv.config();
-const {DataSource}=require('typeorm');
-const myDataSource=new DataSource({
-    type:process.env.TYPEORM_CONNECTION,
-    host:process.env.TYPEORM_HOST,
-    port:process.env.TYPEORM_PORT,
-    username:process.env.TYPEORM_USERNAME,
-    password:process.env.TYPEORM_PASSWORD,
-    database:process.env.TYPEORM_DATABASE
+const { DataSource } = require("typeorm");
+const myDataSource = new DataSource({
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
 });
-myDataSource.initialize().then(()=>{
+myDataSource
+  .initialize()
+  .then(() => {
     console.log("DataSource initialized");
-});
+  })
+  .catch((err) => {
+    console.error("Error", err);
+    myDataSource.destroy();
+  });
 
-const app=express();
+const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
-app.get("/ping",(request, response)=>
-{
-    response.send("pong");
+app.get("/ping", (request, response) => {
+  response.status(201).send("pong");
 });
+//create a user
+app.post("/user", (request, response) => {});
 
-const PORT=process.env.PORT;
+const PORT = process.env.PORT;
 
-app.listen(PORT,()=>
-{
-    console.log(`Server listening on port ${PORT}`);
-}
-);
+const start = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`server listening on ${PORT}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+start();
