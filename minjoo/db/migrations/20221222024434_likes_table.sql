0249ffba35c9dbe7981eb7726c9ae7defb050040
Judy-Choi <mjchoi0831@gmail.com>
2022-12-22 13:24:18 +0900

    브랜치 충돌 해결

---
+++ minjoo/db/migrations/20221222024434_likes_table.sql
@@ -0,0 +1,10 @@
+-- migrate:up
+CREATE TABLE likes (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    post_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- migrate:down
+DROP TABLE likes;
