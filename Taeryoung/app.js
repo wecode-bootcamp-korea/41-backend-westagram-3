863a7280c791bbf9cbd5e2d681d4ef0d2d19f61f
권태령 <taeryoungoori@gmail.com>
2022-12-23 20:35:29 +0900

    Assignment 1 - 초기 환경 세팅 PR

---
+++ Taeryoung/app.js
@@ -0,0 +1,30 @@
+const http = require ("http");
+const express = require ("express");
+const cors = require ("cors");
+const morgan = require ("morgan");
+const dotenv = require("dotenv");
+dotenv.config()
+
+app = express()
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('combined'));
+
+
+// app.get ("/ping", (req, res) => {
+//     res.json({ message : "pong"});
+// });
+
+app.get('/ping', cors(), function (req, res, next) {
+    res.json({message: 'pong'})
+})
+
+const server = http.createServer(app)
+const PORT = process.env.PORT;
+
+const start = async () => {
+    server.listen(PORT, () => console.log(`server is listening on ${PORT}`));
+}
+
+start()
