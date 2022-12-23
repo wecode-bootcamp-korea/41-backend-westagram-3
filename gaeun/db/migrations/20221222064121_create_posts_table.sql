d670a809bfe961b7459f82f13dbbcc03acea52ab
Gaeun Kim <gagaeun@gmail.com>
2022-12-23 15:43:00 +0900

    멘토 리뷰 반영- 모듈 위치 변경 & 캐시 삭제

--- gaeun/db/migrations/20221222064121_create_posts_table.sql
+++ gaeun/db/migrations/20221222064121_create_posts_table.sql
@@ -7,6 +7,7 @@ CREATE TABLE posts (
   user_id INT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP
+  
 );
 
 -- migrate:down
