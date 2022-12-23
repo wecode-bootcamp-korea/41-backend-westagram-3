5a274b9091339b4bb433915987008ec393ac0b0f
kss7082 <118211014+kss7082@users.noreply.github.com>
2022-12-23 17:43:49 +0900

    Add files via upload

---
+++ Seungsu/db/migrations/20221221014837_create_likes_tables.sql
@@ -0,0 +1,11 @@
+-- migrate:up
+create table likes(
+  id int not null auto_increment primary key,
+  user_id int not null,
+  post_id int not null,
+  constraint likes_user_id_fkey foreign key (user_id) references users(id),
+  constraint likes_post_id_fkey foreign key (post_id) references posts(id)
+);
+
+-- migrate:down
+drop table likes;
