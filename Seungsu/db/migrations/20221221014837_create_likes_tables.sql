da57dfe5bfbfd9b69f70f39066bad10027c27203
kss7082 <118211014+kss7082@users.noreply.github.com>
2022-12-26 14:19:21 +0900

    Update 20221221014837_create_likes_tables.sql

--- Seungsu/db/migrations/20221221014837_create_likes_tables.sql
+++ Seungsu/db/migrations/20221221014837_create_likes_tables.sql
@@ -1,7 +1,7 @@
 -- migrate:up
 create table likes(
   id int not null auto_increment primary key,
-  user_id int not null,
+  user_id int not null unique,
   post_id int not null,
   constraint likes_user_id_fkey foreign key (user_id) references users(id),
   constraint likes_post_id_fkey foreign key (post_id) references posts(id)
