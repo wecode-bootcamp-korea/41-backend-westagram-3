fe9115cc08c3d46152b50121e908adb4b73c6e61
권태령 <taeryoungoori@gmail.com>
2022-12-27 13:45:03 +0900

    [권태령]] Assignment 1: Express API - 초기 환경 세팅

--- Taeryoung/app.js
+++ Taeryoung/app.js
@@ -2,29 +2,39 @@
 const express = require ("express");
 const cors = require ("cors");
 const morgan = require ("morgan");
+const { DataSource } = require('typeorm');
 const dotenv = require("dotenv");
 dotenv.config()
 
-app = express()
+const app = express()
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan('combined'));
 
+const myDataSource = new DataSource({
+    type: process.env.TYPEORM_CONNECTION,
+    host: process.env.TYPEORM_HOST,
+    port: process.env.TYPEORM_PORT,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE
+});
 
-// app.get ("/ping", (req, res) => {
-//     res.json({ message : "pong"});
-// });
+myDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
 
-app.get('/ping', cors(), function (req, res, next) {
-    res.json({message: 'pong'})
-})
 
-const server = http.createServer(app)
+    app.get('/ping', cors(), function (req, res, next) {
+        res.json({message: 'pong'})
+    })
+
 const PORT = process.env.PORT;
 
 const start = async () => {
-    server.listen(PORT, () => console.log(`server is listening on ${PORT}`));
+    app.listen(PORT, () => console.log(`server is listening on ${PORT}`));
 }
 
 start()
