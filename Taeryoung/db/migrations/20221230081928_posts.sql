29452cdcbb85be389db16fca5d3e46a6a85db379
권태령 <taeryoungoori@gmail.com>
2022-12-30 17:45:01 +0900

    Assignment 3 - 게시글 등록하기

--- Taeryoung/db/migrations/20221230081928_posts.sql
+++ Taeryoung/db/migrations/20221230081928_posts.sql
@@ -4,11 +4,10 @@ CREATE TABLE posts (
     title VARCHAR(100) NOT NULL,
     content VARCHAR(100) NOT NULL,
     user_id INT NOT NULL,
-    imageUrl VARCHAR(2000) NOT NULL,
+    image_url VARCHAR(2000) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
     updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp,
-    CONSTRAINT created_posts_create_users FOREIGN KEY (user_id) REFERENCES users (id)
-)
-
+    FOREIGN KEY (user_id) REFERENCES users (id)
+)  
 -- migrate:down
 DROP TABLE posts;
--- Taeryoung/db/migrations/20221230082127_users_unique_key.sql
+++ Taeryoung/db/migrations/20221230082127_users_unique_key.sql
@@ -1,5 +1,5 @@
 -- migrate:up
 ALTER TABLE users ADD UNIQUE email_unique (email);
 
-
 -- migrate:down
+
